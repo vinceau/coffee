d8932eb530db2ef80a262e5f4ee379f4815afd36
Vince Au <vinceau09@gmail.com>
2024-02-01 19:40:58 +1100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+APP = ['coffee.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'iconfile': 'icon.icns',
+    'plist': {
+        'CFBundleShortVersionString': '0.2.0',
+        'LSUIElement': True,
+    },
+    'includes': ['rumps', 'pyatuogui'],
+    'excludes': ['rubicon']
+}
+
+setup(
+    app=APP,
+    name='Coffee',
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'], install_requires=['rumps', 'pyautogui']
+)
