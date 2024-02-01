dde91c12286ade14cf4bcaae9c959e60dd0322b2
Vince Au <vinceau09@gmail.com>
2024-02-01 19:21:34 +1100

    make it a single start/stop button

--- coffee.py
+++ coffee.py
@@ -7,19 +7,15 @@ class CoffeeApp(object):
         self.config = {
             "app_name": "Coffee",
             "start": "Start Timer",
-            "pause": "Pause Timer",
-            "continue": "Continue Timer",
             "stop": "Stop Timer",
-            "break_message": "Time is up! Take a break :)",
             "interval": 10,
         }
         self.app = rumps.App(self.config["app_name"])
         self.timer = rumps.Timer(self.on_tick, 1)
         self.interval = self.config["interval"]
         self.set_up_menu()
-        self.start_pause_button = rumps.MenuItem(title=self.config["start"], callback=self.start_timer)
-        self.stop_button = rumps.MenuItem(title=self.config["stop"], callback=None)
-        self.app.menu = [self.start_pause_button, self.stop_button]
+        self.start_stop_button = rumps.MenuItem(title=self.config["start"], callback=self.start_timer)
+        self.app.menu = [self.start_stop_button, self.stop_button]
 
     def set_up_menu(self):
         self.timer.stop()
@@ -47,20 +43,19 @@ class CoffeeApp(object):
         self.timer.end = self.interval
 
     def start_timer(self, sender):
-        if sender.title.lower().startswith(("start", "continue")):
-            if sender.title == self.config["start"]:
-                self.timer.count = 0
-                self.timer.end = self.interval
-            sender.title = self.config["pause"]
+        if sender.title.lower().startswith("start"):
+            self.timer.count = 0
+            self.timer.end = self.interval
+            sender.title = self.config["stop"]
             self.timer.start()
         else:
-            sender.title = self.config["continue"]
+            sender.title = self.config["start"]
             self.timer.stop()
 
     def stop_timer(self, sender):
         self.set_up_menu()
         self.stop_button.set_callback(None)
-        self.start_pause_button.title = self.config["start"]
+        self.start_stop_button.title = self.config["start"]
 
     def run(self):
         self.app.run()
